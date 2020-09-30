5ea58017fa5a47647d4558519d6eb97e4497aa0d
jainrishabh18 <49811626+jainrishabh18@users.noreply.github.com>
2020-09-30 16:11:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Subtraction in Java.
---
+++ substract.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Rishabh {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int x = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int y = scanner.nextInt();
+        System.out.println("The subtraction is: "+(x-y));
+    }
+}
